53c06ca1ba6433f9c415f38e70462122a8afa0bb
Edbert Santoso <edbertsantoso14@gmail.com>
2020-05-15 17:55:10 +0800

    DocuBot search function v.1.0
    Able to find the most similar content online, with a .docxfile as an input

--- analyzer.py
+++ analyzer.py
@@ -4,6 +4,13 @@ import statistics
 from docx import Document
 from nltk.corpus import wordnet as wn
 
+def getText(filename):
+    doc = Document(filename)
+    for p in doc.paragraphs:
+        if p.text != '':
+            fullText.append(p.text)
+    return ' '.join(fullText)
+
 def tag_conversion(penntag):
     morphy_tag = {'NN':wn.NOUN, 'JJ':wn.ADJ,
                   'VB':wn.VERB, 'RB':wn.ADV}
@@ -13,12 +20,18 @@ def tag_conversion(penntag):
         return None
 
 def doc_to_synsets(doc):
-    
-    token = nltk.word_tokenize(doc)
+    Text = getText(doc)
+    token = word_tokenize(Text)
     tag = nltk.pos_tag(token)
     wn_tag = [(i[0], tag_conversion(i[1])) for i in tag]
     output = [wn.synsets(i, z)[0] for i, z in wn_tag if len(wn.synsets(i, z))>0]
-    print(output)
+    return output
+
+def str_to_synsets(s1):
+    token = word_tokenize(s1)
+    tag = nltk.pos_tag(token)
+    wn_tag = [(i[0], tag_conversion(i[1])) for i in tag]
+    output = [wn.synsets(i, z)[0] for i, z in wn_tag if len(wn.synsets(i, z))>0]
     return output
 
 def similarity_ratio(s1,s2):
@@ -27,15 +40,19 @@ def similarity_ratio(s1,s2):
     for s in s1:
         testList.append(max([i.path_similarity(s) for i in s2 if i.path_similarity(s) is not None]))
         if(i.path_similarity(s) for i in s2 if i.path_similarity(s) is None):
-            return 0
+            continue
 
     output = sum(testList)/len(testList)
     return output
 
-def analyze_similarity(d1, d2):
-
+def doc_similarity(d1, d2):
     synsets1 = doc_to_synsets(d1)
     synsets2 = doc_to_synsets(d2)
 
-    return(similarity_ratio(synsets1,synsets2) + similarity_ratio(synsets2, synsets1)) /2
+    return ((similarity_ratio(synsets1,synsets2) + similarity_ratio(synsets2, synsets1)) /2)
 
+def query_similarity(s1, s2):
+    synsets1 = str_to_synsets(s1)
+    synsets2 = str_to_synsets(s2)
+
+    return((similarity_ratio(synsets1, synsets2) + similarity_ratio(synsets2, synsets1)) / 2)
--- search_engine.py
+++ search_engine.py
@@ -3,8 +3,20 @@ import pandas as pd
 import os
 from dotenv import load_dotenv, find_dotenv
 from nltk import word_tokenize
+from docx import Document
+import analyzer
 
 class SearchEngine():        
+
+    def getQuery(self, filename):
+        doc = Document(filename)
+        fullText = ''
+        for p in doc.paragraphs:
+            if p != '':
+                Text = (p.text)
+                fullText += (Text + " ")
+        
+        return fullText
 
     def searchWeb(self, queryInput):
         load_dotenv(find_dotenv())
@@ -14,7 +26,6 @@ class SearchEngine():
         
         query = queryInput
         url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}"
-
         data = requests.get(url).json()
 
         results_item = data.get("items")
@@ -32,56 +43,34 @@ class SearchEngine():
                 link = results_item.get("link")
 
                 if(title != '' and snippet != '' and html_snippet != '' and link != ''):
-                    print("="*10, f"Result #{i}", "="*10)
-                    print("Title: ", title)
-                    print("Description: ", snippet)
-                    print("HtmlDescription: ", html_snippet)
-                    print("URL: ", link, "\n")
-
                     titles.append(title)
                     snippets.append(snippet)
                     html_snippets.append(html_snippet)
                     links.append(link)
             except:
                 continue
-        
-        df = pd.DataFrame()
-        df['Title'] = titles
-        df['Descriptions'] = snippets
-        df['Html Snippets'] = html_snippets
-        df['Links'] = links
-        df['Query'] = query
-        df.to_csv(r'csvFiles\google.csv')
 
-        return titles, snippets, links, query
+        best_score = 0.0
+        best_link = ''
 
-    def getResults(self):
-        try:
-            queryFile = open(r"csvFiles\query.csv")
-            titles = []
-            descs = []
-            links = []
-            queries = []
-            if(queryFile):
-               data =  pd.read_csv(r"csvFiles\query.csv")
+        for i, j in zip(snippets,links):
+            simi = analyzer.query_similarity(i, query)
+            if(simi > best_score):
+                best_score = simi
+                best_link = j
 
-               for w in data['WORDS']:
-                   se = SearchEngine()
-                   title, desc, link, query = se.searchWeb(w)
-                   titles.append(title)
-                   descs.append(desc)
-                   links.append(link)
-                   queries.append(query)
+        return best_link, best_score
+
+    def get_simi_link(self, d):
             
-            data['Title'] = titles
-            data['Descriptions'] = descs
-            data['Links'] = links
-            data['Query'] = queries
-            data.drop(['Unnamed: 0'], axis = 1)
-            data.to_csv('csvFiles\results.csv')
-
-        except IOError:
-            print("File not found, please input a document to check first")
+        se = SearchEngine()
+        doc = se.getQuery(d)
+        link_result, score = se.searchWeb(doc)
+        try:
+            if(link_result != ''):
+                return link_result, score
+        except:
+            return "No similar content found online"
 
 a = SearchEngine()
-a.getResults()
+print(a.get_simi_link('docxFiles\sample.docx'))
