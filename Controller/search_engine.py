f17c81434a4dabe5291f2c3ba51f658da17b35ba
Edbert Santoso <edbertsantoso14@gmail.com>
2020-05-24 18:02:10 +0800

    MVC Architecture Implementation

---
+++ Controller/search_engine.py
@@ -0,0 +1,101 @@
+import requests
+import os
+from dotenv import load_dotenv, find_dotenv
+from nltk import word_tokenize, sent_tokenize
+from docx import Document
+from Model import doc_reader
+
+class SearchEngine():        
+
+    """
+    def doc_to_paraset(self, filename):
+        doc = Document(filename)
+        fullText = []
+        for p in doc.paragraphs:
+            if p != '':
+                fullText.append(p.text)
+        fullText = [ft for ft in fullText if ft]
+        return ",".join(fullText)
+    """
+
+    def searchWeb(self, qInput):
+        """
+        Function to search the web using paragraphs from the docx files as a query, returning at most 10 results for each paragraphs,
+        It then compares each results with query and returns only the most similar result
+        """
+        
+        #Read the .env files to get API Key and Search engine ID
+        load_dotenv(find_dotenv())
+        API_KEY = os.getenv("API_KEY4")
+        SEARCH_ENGINE_ID = os.getenv("SEARCH_ENGINE_ID4")
+
+        #Create an object of the doc_reader class to compare similarities
+        dr = doc_reader.DocReader()
+        
+        #Initiale lists that stores paragraphs for queries, most similar links, and total score respectively
+        queryInput = qInput
+        result_links = []
+        result_scores = 0.0
+
+        #Iteratively finds best results for each paragraphs in the document
+        for q in range(len(queryInput)):
+            query = queryInput[q]
+            url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}"
+            data = requests.get(url).json()
+
+            #Get the results returned from the search engine
+            results_item = data.get("items")
+            #If empty then skip
+            if results_item == None:
+                continue
+            snippets = []
+            links = []
+
+            #Appends link and descriptions from the search results into a list
+            for rs in results_item:
+                try:
+                    snippet = rs.get("snippet")
+                    link = rs.get("link")
+
+                    snippets.append(snippet)
+                    links.append(link)
+                except:
+                    continue
+            
+            #Temp variable to store highest similarity and most similar link
+            best_score = 0.0
+            best_link = ''
+
+            #Iterate through the descriptions and compare with query
+            #Replace current best if score is higher than the current best
+            for i in range(len(snippets)):
+                simi = dr.query_similarity(links[i], query)
+                if(simi > best_score):
+                    best_score = simi
+                    best_link = links[i]
+
+            result_links.append(best_link)
+            result_scores += best_score
+        
+        #Returns the total score and most similar links to each paragraphs
+        return((result_scores/len(result_links)) * 100), result_links
+
+    def get_simi_link(self, d):
+
+        """
+        Take in a document file and find the most similar contents online
+        Based on the content of each paragraphs
+        """
+
+        dr = doc_reader.DocReader()
+        se = SearchEngine()
+        doc = dr.getQuery(d)
+        link_result, score = se.searchWeb(doc)
+        try:
+            if(link_result != '' and score != 0.0):
+                return link_result, score
+        except:
+            return "No similar content found online"
+
+a = SearchEngine()
+print(a.get_simi_link('docxFiles\sample.docx'))
---
+++ Model/doc_reader.py
@@ -0,0 +1,110 @@
+from nltk import word_tokenize, sent_tokenize, pos_tag
+import nltk
+from docx import Document
+from nltk.corpus import wordnet as wn
+
+class DocReader():
+
+    def getText(self, filename):
+        """
+        Return docx files into one set of string to be tokenized later
+        """
+
+        doc = Document(filename)
+        fullText = []
+        for p in doc.paragraphs:
+            if p.text != '':
+                fullText.append(p.text)
+        return ' '.join(fullText)
+
+    def getQuery(self, filename):
+        """
+        Read the docx files and separate the content into paragraphs
+        Return content of each paragraphs in a list to be used for
+        Search functions later
+        """
+
+        doc = Document(filename)
+        fullText = []
+        for p in doc.paragraphs:
+            if p != '':
+                fullText.append(p.text)
+        fullText = [ft for ft in fullText if ft]
+        return(fullText)
+
+    def tag_conversion(self, penntag):
+        """
+        Convert penn tree tag into a WordNet Tag
+        """
+        morphy_tag = {'NN':wn.NOUN, 'JJ':wn.ADJ,
+                    'VB':wn.VERB, 'RB':wn.ADV}
+        try:
+            return morphy_tag[penntag[:2]]
+        except KeyError:
+            return None
+
+    def doc_to_synsets(self, doc):
+        """
+        Getting text from a document file to be tokenized and separated into words,
+        WordNet is then used to find synonym sets for each words to be used for comparisons later
+        """
+        dr = DocReader()
+        Text = dr.getText(doc)
+        token = word_tokenize(Text)
+        tag = nltk.pos_tag(token)
+        wn_tag = [(i[0], dr.tag_conversion(i[1])) for i in tag]
+        output = [wn.synsets(i, z)[0] for i, z in wn_tag if len(wn.synsets(i, z))>0]
+        return output
+
+    def str_to_synsets(self, s1):
+        """
+        Instead of reading from a docx files, it reads in strings and
+        returns a the sets of synonyms.
+        """
+
+        dr = DocReader()
+        token = word_tokenize(s1)
+        tag = nltk.pos_tag(token)
+        wn_tag = [(i[0], dr.tag_conversion(i[1])) for i in tag]
+        output = [wn.synsets(i, z)[0] for i, z in wn_tag if len(wn.synsets(i, z))>0]
+        return output
+
+    def similarity_ratio(self, s1,s2):
+        """
+        S1 = Synonym sets of input of documents/queries
+        S2 = Synonym sets of text from docs/queries to be compared with
+
+        Compares synonym sets of S1 and S2 and returns the highest comparison score
+        """
+        simis = []
+
+        for s in s1:
+            simlist = [s.path_similarity(ss) for ss in s2 if s.path_similarity(ss) is not None]
+            if not simlist:
+                continue
+
+            max_score = max(simlist)
+            simis.append(max_score)
+
+        output = sum(simis)/len(simis)
+        return output
+
+    def doc_similarity(self, d1, d2):
+        """
+        Function used to compare simiarity between 2 docx fiels
+        """
+        dr = DocReader()
+        synsets1 = dr.doc_to_synsets(d1)
+        synsets2 = dr.doc_to_synsets(d2)
+
+        return ((dr.similarity_ratio(synsets1,synsets2) + dr.similarity_ratio(synsets2, synsets1)) /2)
+
+    def query_similarity(self, s1, s2):
+        """
+        Function used to compare queries, can also be used for sentences/string comparison
+        """
+        dr = DocReader()
+        synsets1 = dr.str_to_synsets(s1)
+        synsets2 = dr.str_to_synsets(s2)
+
+        return((dr.similarity_ratio(synsets1, synsets2) + dr.similarity_ratio(synsets2, synsets1)) / 2)
